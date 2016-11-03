7385058c0aade0c8b00c542590b8c3c18499b4a5
efremfilho <efremdx@yahoo.com.br>
2016-11-02 21:32:49 -0300

    inclui carrossel

---
+++ js/slick_init.js
@@ -0,0 +1,11 @@
+ $(document).ready(function(){
+      $('.autoplay').slick({
+        dots: true,
+  		
+        slidesToShow: 1,
+  		slidesToScroll: 1,
+  		autoplay: true,
+  		autoplaySpeed: 1500,
+	});
+  });
+
